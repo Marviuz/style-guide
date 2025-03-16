db05d6160d5c4e9a4249d974dbfc993f840e8884
Marviuz <marviuz.gaming@gmail.com>
2025-03-16 22:31:59 +0800

    docs: add tailwind

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -2,10 +2,11 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import contentCollections from '@content-collections/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), contentCollections()],
+  plugins: [react(), tailwindcss(), contentCollections()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
