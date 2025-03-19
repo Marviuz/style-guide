103c155538e41e3b3ee416b0006bcf136ffb8d69
Marviuz <marviuz.gaming@gmail.com>
2025-03-20 00:39:41 +0800

    fix: attempt to fix changesets

--- apps/docs/prettier.config.mjs
+++ apps/docs/prettier.config.mjs
@@ -3,5 +3,8 @@ import marviuzConfig from '@marviuz/prettier-config';
 /** @type {import('prettier').Config} */
 export default {
   ...marviuzConfig,
-  plugins: [...marviuzConfig.plugins, 'prettier-plugin-tailwindcss'],
+  plugins: [
+    ...marviuzConfig.plugins,
+    import.meta.resolve('prettier-plugin-tailwindcss'),
+  ],
 };
