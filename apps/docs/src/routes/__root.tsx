e565d41dbf0156f1f662ecb0a81f9e5249d40748
Marviuz <marviuz.gaming@gmail.com>
2025-03-20 00:30:21 +0800

    feat: docs website meta

--- apps/docs/src/routes/__root.tsx
+++ apps/docs/src/routes/__root.tsx
@@ -1,15 +1,36 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { createRootRoute, HeadContent, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      { charSet: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { title: 'Marviuz Style Guide' },
+      { name: 'og:title', content: 'Marviuz Style Guide' },
+      {
+        name: 'og:description',
+        content:
+          'Yet another set of configs for your JavaScript/TypeScript projects',
+      },
+      {
+        name: 'og:image',
+        content: 'https://style-guide.marviuz.me/opengraph-image.jpg',
+      },
+      { name: 'og:image:alt', content: 'A preview of the Marviuz Style Guide' },
+      { name: 'og:url', content: 'https://style-guide.marviuz.me' },
+      { name: 'og:type', content: 'website' },
+    ],
+  }),
   component: Root,
 });
 
 function Root() {
   return (
     <>
+      <HeadContent />
       <Header />
       <Outlet />
       <Footer />
--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import contentCollections from '@content-collections/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     contentCollections(),
+    Sitemap({ hostname: 'https://style-guide.marviuz.me' }),
   ],
   resolve: {
     alias: {
